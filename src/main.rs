317136be6ec3fd1489848ef0fc98b31bc5cd6813
brian <brianjsummerfield@gmail.com>
2023-12-08 16:39:48 -0600

    split on cookie

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,14 @@ use axum::{
     BoxError, Router,
 };
 use http::StatusCode;
-use maud::html;
+use maud::{html, PreEscaped};
 use serde::{Deserialize, Serialize};
 use tower::ServiceBuilder;
 use tower_sessions::{Expiry, MemoryStore, Session, SessionManagerLayer};
 
 const USERNAME_KEY: &str = "username";
 
-#[derive(Default, Deserialize, Serialize, Debug)]
+#[derive(Default, Deserialize, Serialize, Debug, Clone)]
 struct Username {
     username: String,
 }
@@ -47,60 +47,89 @@ async fn main() {
 
 async fn handle_submit_username(
     session: Session,
-    Form(username): Form<Username>,
+    Form(username_struct): Form<Username>,
 ) -> impl IntoResponse {
+    let username = username_struct.username;
     println!("{:?}", session);
     session
         .insert(USERNAME_KEY, &username)
         .expect("Could not serialize.");
-    render_username(username).into_response()
+
+    Html(username_body(&username)).into_response() // Pass the username string directly
 }
 
 async fn handler(session: Session) -> impl IntoResponse {
     println!("{:?}", session);
-    match session.get::<Username>(USERNAME_KEY) {
-        Ok(Some(username)) => render_username(username).into_response(), // Handle the case where a username is present
-        Ok(None) => render_enter_username().await.into_response(), // Handle the case where a username is not present
-        Err(_) => StatusCode::INTERNAL_SERVER_ERROR.into_response(), // Simplified error handling
+    match session.get::<String>(USERNAME_KEY) {
+        Ok(Some(username)) => render_base(&username).into_response(),
+        Ok(None) => render_base_no_username().into_response(),
+        Err(_) => StatusCode::INTERNAL_SERVER_ERROR.into_response(),
     }
 }
 
-fn render_username(username: Username) -> String {
-    format!("Hello, {}!", username.username)
+fn username_body(username: &str) -> String {
+    let markup = html! {
+        h1 { "Hello, " (username) "!" }
+    };
+
+    markup.into_string()
 }
 
-async fn render_enter_username() -> Html<String> {
+fn render_base_no_username() -> Html<String> {
     let markup = html! {
         (maud::DOCTYPE)
         html {
-            head {
-                title { "Server Test" }
-                script
-                    src="https://unpkg.com/htmx.org@1.9.9"
-                    integrity="sha384-QFjmbokDn2DjBjq+fM+8LUIVrAgqcNW2s0PjAxHETgRn9l4fvX31ZxDxvwQnyMOX"
-                    crossorigin="anonymous" {}
-            }
-            body {
-                h1 { "Enter Your Username" }
-                form
-                    hx-post="/submit_username"
-
-                {
-                    input
-                        type="text"
-                        name="username"
-                        placeholder="Username"
-                        required="true"
-                    {}
-                    button
-                        type="submit"
-                    {
-                        "Submit"
-                    }
-                }
-
-            }
+        head {(header())}
+            body {(PreEscaped(username_form_body().into_string()))}
         }
     };
     Html(markup.into_string())
+}
+
+fn render_base(username: &str) -> Html<String> {
+    let markup = html! {
+        (maud::DOCTYPE)
+        html {
+        head {
+            (header())
+        }
+            body {(PreEscaped(username_body(username)))}
+        }
+    };
+    Html(markup.into_string())
+}
+
+fn header() -> PreEscaped<String> {
+    let markup = html! {
+        head {
+            title { "Server Test" }
+            script
+                src="https://unpkg.com/htmx.org@1.9.9"
+                integrity="sha384-QFjmbokDn2DjBjq+fM+8LUIVrAgqcNW2s0PjAxHETgRn9l4fvX31ZxDxvwQnyMOX"
+                crossorigin="anonymous" {}
+        }
+    };
+    PreEscaped(markup.into_string())
+}
+fn username_form_body() -> PreEscaped<String> {
+    let markup = html! {
+        h1 { "Enter Your Username" }
+        form
+            hx-post="/submit_username"
+            hx-target="body"
+        {
+            input
+                type="text"
+                name="username"
+                placeholder="Username"
+                required="true"
+            {}
+            button
+                type="submit"
+            {
+                "Submit"
+            }
+        }
+    };
+    PreEscaped(markup.into_string())
 }
